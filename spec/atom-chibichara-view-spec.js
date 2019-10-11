15115e8aaf413b6e997a4f2fb939d4f30186170e
safu9 <safu9.dev@gmail.com>
2019-10-12 08:45:47 +0900

    Initial commit

---
+++ spec/atom-chibichara-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomChibicharaView from '../lib/atom-chibichara-view';
+
+describe('AtomChibicharaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
