fc7fae2c29f4592eef886b524a88569135be691d
safu9 <safu9.dev@gmail.com>
2019-10-13 21:47:08 +0900

    Remove tests

--- spec/atom-chibichara-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomChibicharaView from '../lib/atom-chibichara-view';
-
-describe('AtomChibicharaView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
